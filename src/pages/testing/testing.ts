4497ec5c97869db25e860ffdb461aadcbf866821
gssantost <31294447+gssantost@users.noreply.github.com>
2018-12-04 20:35:02 -0400

    updates

--- src/pages/testing/testing.ts
+++ src/pages/testing/testing.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { TableProvider } from '../../providers/table/table';
 import { MessageController } from '../../utils/messageCtrl/messageCtrl';
-import { BluetoothProvider } from '../../providers/bluetooth/bluetooth';
 import { BluetoothSerial } from '@ionic-native/bluetooth-serial';
+import { Observable } from 'rxjs';
+import { ISubscription } from 'rxjs/Subscription';
 
 /**
  * Generated class for the TestingPage page.
@@ -20,6 +21,8 @@ import { BluetoothSerial } from '@ionic-native/bluetooth-serial';
 export class TestingPage {
 
   public userInput: string;
+  private observable: Observable<any>;
+  private suscription: ISubscription;
 
   constructor(
     public navCtrl: NavController, 
@@ -34,9 +37,6 @@ export class TestingPage {
     console.log('ionViewDidLoad TestingPage');
   }
 
-  send() {
-    
-  }
 
   check(id: string) {
     this.uiCtrl.load();
@@ -46,11 +46,20 @@ export class TestingPage {
         console.log("IC seleccionado", JSON.stringify(data))
 
         const { id, result, info, ...rest } = data;
-        let message = {
+        let obj = {
           ...rest
         };
+
+        let message = JSON.stringify(obj) + '\n';
+
+        /*this.suscription = this.createSocket(message)
+          .suscribe((data) => {
+            console.log(data);
+            this.uiCtrl.dismiss();
+          }
+        );*/  
         
-        this.bluetoothSerial.write(JSON.stringify(message) + '\n')
+        this.bluetoothSerial.write(message)
         .then((success) => {
               console.log(success); 
               this.uiCtrl.dismiss();
@@ -59,14 +68,37 @@ export class TestingPage {
           this.uiCtrl.show("Error", failure);
           this.uiCtrl.dismiss();
         });
+
       }).catch((e) => {
         console.log(e)
         this.uiCtrl.show("Error", "IC not found.")
         this.uiCtrl.dismiss();
       });
-    }
-
-  suscribeData() {
-    this.bluetoothSerial.subscribe('\n');
   }
+
+  /*createSocket(message: string) {
+    return Observable.create((observer) => {
+      this.bluetoothSerial.isConnected()
+        .then((success) => {
+
+          console.log('Connected!', success);
+
+          this.observable = Observable.from(this.bluetoothSerial.write(message))
+            .flatMap(() => this.bluetoothSerial.subscribeRawData())
+            .flatMap(() => this.bluetoothSerial.readUntil('\n'));
+
+            this.observable.subscribe(data => {
+              observer.next(data);
+            });
+
+        }, (failure) => {
+
+          console.log('Not connected', failure);
+          observer.next('Not connected');
+          observer.complete();
+        
+        })
+    });
+  }*/
+
 }
--- src/providers/table/tableData.ts
+++ src/providers/table/tableData.ts
@@ -5,8 +5,8 @@ export const Table: Chip[] = [
     id: '74LS04',
     pinNo: 14,
     config: 'OIOIOIGIOIOIOV',
-    test: [ [0], [1] ],
-    result: [ [0], [1] ],
+    test: [[0], [1]],
+    result: [[0], [1]],
     info: 'NOT - 6 Compuertas Inversoras'
   },
   {
@@ -16,14 +16,32 @@ export const Table: Chip[] = [
     test: [[0,0], [0,1], [1,0], [1,1]],
     result: [[0,0,0,1]], 
     info: 'AND - 4 Compuertas'
-  }
+  },
+  {
+    id: '74LS02',
+    pinNo: 14,
+    config: 'OOIOOIGIOOIOOV',
+    test: [[0,0], [0,1], [1,0], [1,1]],
+    result: [[0, 1, 1, 1]],
+    info: 'OR - 4 Compuertas de 2 Entradas'
+  },
+  {
+    id: '74LS00',
+    pinNo: 14,
+    config: 'OOIOOIGIOOIOOV',
+    test: [[0, 0], [0, 1], [1, 0], [1, 1]],
+    result: [[0, 1, 1, 1]],
+    info: 'OR - 4 Compuertas de 2 Entradas'
+  },
 ];
 
-/*{
+/*
+  {
     id: '74LS04',
     pinNo: 14,
     config: 'OIOIOIGIOIOIOV',
     test: [ [0, 1], [0, 1], [0, 1], [0, 1], [0, 1], [0, 1] ],
     result: [ [1, 0], [1, 0], [1, 0], [1, 0], [1, 0], [1, 0] ],
     info: '6 Compuertas Inversoras NOT'
-  }*/
+  }
+*/
